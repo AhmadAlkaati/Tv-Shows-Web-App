b4ab5809f59cc2c14a3cd20aeab59952a55bfe2d
ahmaddimashki <47057524+ahmaddimashki@users.noreply.github.com>
2021-04-16 19:23:56 +0200

    Uploading all files.

---
+++ js/app.js
@@ -0,0 +1,126 @@
+function main() {
+  const showsMain = document.querySelector('.shows');
+
+  const searchInput = document.querySelector('.search input');
+
+  function fetchShows() {
+    fetch('http://api.tvmaze.com/shows')
+      .then((shows) => {
+        return shows.json();
+      })
+      .then((shows) => {
+        displayShows(shows);
+      });
+  }
+
+  fetchShows();
+
+  function displayShows(shows) {
+    console.log(shows);
+    let showDiv;
+    shows.forEach((show) => {
+      let showCover;
+      let showInfo;
+      let showName;
+      let showRating;
+      let showReleaseDate;
+      let showSummary;
+      if (document.title == 'Tv Shows')
+        if (show.id <= 40) {
+          // console.log(show.id);
+          showCover = `<img src = '${show.image.medium}' class = 'show-cover'> </img>`;
+          showName = `<h3 class = 'show-name'> ${show.name} </h3>`;
+          showRating = `<h3 class = 'show-rating'> ${show.rating.average} </h3> `;
+          showReleaseDate = `<h5 class = 'show-date'> ${show.premiered} </h5>`;
+          showInfo = `<div class = 'show-info'> ${showName} ${showRating} ${showReleaseDate} </div> `;
+          showSummary = ` <div class = 'show-summary'> <h3> Overview </h3> ${show.summary} </div>`;
+          showDiv = `<div class = 'show'> ${showCover} ${showInfo} ${showSummary}  </div>`;
+          showsMain.innerHTML += showDiv;
+        }
+      if (document.title == 'Page 2') {
+        console.log('ok');
+        if (show.id <= 80 && show.id > 40) {
+          // console.log(show.id);
+          showCover = `<img src = '${show.image.medium}' class = 'show-cover'> </img>`;
+          showName = `<h3 class = 'show-name'> ${show.name} </h3>`;
+          showRating = `<h3 class = 'show-rating'> ${show.rating.average} </h3> `;
+          showReleaseDate = `<h5 class = 'show-date'> ${show.premiered} </h5>`;
+          showInfo = `<div class = 'show-info'> ${showName} ${showRating} ${showReleaseDate} </div> `;
+          showSummary = ` <div class = 'show-summary'> <h3> Overview </h3> ${show.summary} </div>`;
+          showDiv = `<div class = 'show'> ${showCover} ${showInfo} ${showSummary}  </div>`;
+          showsMain.innerHTML += showDiv;
+        }
+      }
+      if (document.title == 'Page 3') {
+        console.log('ok');
+        if (show.id <= 120 && show.id > 80) {
+          // console.log(show.id);
+          showCover = `<img src = '${show.image.medium}' class = 'show-cover'> </img>`;
+          showName = `<h3 class = 'show-name'> ${show.name} </h3>`;
+          showRating = `<h3 class = 'show-rating'> ${show.rating.average} </h3> `;
+          showReleaseDate = `<h5 class = 'show-date'> ${show.premiered} </h5>`;
+          showInfo = `<div class = 'show-info'> ${showName} ${showRating} ${showReleaseDate} </div> `;
+          showSummary = ` <div class = 'show-summary'> <h3> Overview </h3> ${show.summary} </div>`;
+          showDiv = `<div class = 'show'> ${showCover} ${showInfo} ${showSummary}  </div>`;
+          showsMain.innerHTML += showDiv;
+        }
+      }
+
+      if (document.title == 'Page 4') {
+        console.log('ok');
+        if (show.id <= 160 && show.id > 120) {
+          // console.log(show.id);
+          showCover = `<img src = '${show.image.medium}' class = 'show-cover'> </img>`;
+          showName = `<h3 class = 'show-name'> ${show.name} </h3>`;
+          showRating = `<h3 class = 'show-rating'> ${show.rating.average} </h3> `;
+          showReleaseDate = `<h5 class = 'show-date'> ${show.premiered} </h5>`;
+          showInfo = `<div class = 'show-info'> ${showName} ${showRating} ${showReleaseDate} </div> `;
+          showSummary = ` <div class = 'show-summary'> <h3> Overview </h3> ${show.summary} </div>`;
+          showDiv = `<div class = 'show'> ${showCover} ${showInfo} ${showSummary}  </div>`;
+          showsMain.innerHTML += showDiv;
+        }
+      }
+      if (document.title == 'Page 5') {
+        console.log('ok');
+        if (show.id <= 200 && show.id > 160) {
+          // console.log(show.id);
+          showCover = `<img src = '${show.image.medium}' class = 'show-cover'> </img>`;
+          showName = `<h3 class = 'show-name'> ${show.name} </h3>`;
+          showRating = `<h3 class = 'show-rating'> ${show.rating.average} </h3> `;
+          showReleaseDate = `<h5 class = 'show-date'> ${show.premiered} </h5>`;
+          showInfo = `<div class = 'show-info'> ${showName} ${showRating} ${showReleaseDate} </div> `;
+          showSummary = ` <div class = 'show-summary'> <h3> Overview </h3> ${show.summary} </div>`;
+          showDiv = `<div class = 'show'> ${showCover} ${showInfo} ${showSummary}  </div>`;
+          showsMain.innerHTML += showDiv;
+        }
+      }
+    });
+    const allShows = document.querySelectorAll('.show');
+
+    function filterShows() {
+      console.log(allShows);
+      const showNameElements = document.querySelectorAll('.show-name');
+      searchInput.addEventListener('input', function (e) {
+        const smallLetters = e.target.value.toLowerCase();
+        console.log(smallLetters);
+        allShows.forEach((show) => {
+          const showName = show.children[1].children[0];
+          const showNameText = showName.textContent.trim();
+          console.log(showNameText.toLowerCase().indexOf(smallLetters));
+          // Filtering based on first letter and then on rest of other letters//
+          if (showNameText.toLowerCase().indexOf(smallLetters) == 0) {
+            show.style.display = 'flex';
+            console.log('ok');
+          } else {
+            show.style.display = 'none';
+          }
+        });
+      });
+    }
+    filterShows();
+  }
+}
+
+main();
+
+console.log(location);
---
+++ js/dynamic-content.js
@@ -0,0 +1,17 @@
+document.write(`      <main>
+<header><h1>Extra TV</h1></header>
+<section class="search">
+  <input type="text" placeholder="search a show" />
+</section>
+<section class="shows"></section>
+<section class="pagination">
+  <a href="/index.html">1</a><a href="../page2/page2.html">2</a
+  ><a href="../page3/page3.html">3</a><a href="../page4/page4.html">4</a
+  ><a href="../page5/page5.html">5</a>
+</section>
+<div class="social">
+<a href="https://github.com/AhmadAlkaati/Tv-Shows-Web-App"
+  ><i class="fab fa-github"></i>Github repository</a
+>
+</div>
+</main>`);
